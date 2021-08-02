bce816d8112c439d4d85b8cedf160433511f35ae
Lazar <46060203+Lazzaro98@users.noreply.github.com>
2021-08-03 01:09:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = pymongo.MongoClient(os.getenv("MONGODB_CONNECTION_URL"))
-db = client.Corax  # ! Change this line to the correct collection name
-collection = db.ctftime_history  # ! Change this to be the correct table name
+client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.anjon.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mydb = client["testdbname"]
+collection = mydb["testcollectionname"]
 
-TEAM_ID = "113107"
 
-HEADERS = {"User-Agent": "Corax"}
+TEAM_ID = "137561"
 
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+HEADERS = {"User-Agent": "Cluster0"}
+
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/871858702695678042/oztUEs7wBJ5bVp9UI-fKBBNtBdZDKj17zQGA9ysHULEJ-4ljT2PndNBcn2tXG3jUyOog"
 PFP_URL = "https://cdn.discordapp.com/attachments/719605546101113012/731453497479790672/ctftime.png"
 
 
@@ -28,7 +29,8 @@ def scrape_website(team_id):
     """Downloads the website and scapes the rating"""
     page = requests.get(f"https://ctftime.org/team/{team_id}", headers=HEADERS)
     soup = BeautifulSoup(page.content, "html.parser")
-    rating_div = soup.find(id="rating_2020")
+    print(soup)
+    rating_div = soup.find(id="rating_2021")
     rating_a = str(rating_div.select('a[href="/stats/NO"]')[0])
     rating_a = rating_a.replace('<a href="/stats/NO">', "").replace("</a>", "")
     return int(rating_a)
@@ -39,7 +41,7 @@ def get_world_rating(team_id):
     response = requests.get(
         f"https://ctftime.org/api/v1/teams/{team_id}/", headers=HEADERS)
     print(response)
-    return int(response.json()["rating"][0]["2020"]["rating_place"])
+    return int(response.json()["rating"][0]["2021"]["rating_place"])
 
 
 def post_discord_message(data):
@@ -52,19 +54,21 @@ def main():
     last_entry = collection.find_one(sort=[("_id", pymongo.DESCENDING)])
     try:
         last_rating = {
-            "world": int(last_entry["world"]),
-            "region": int(last_entry["region"])
+            "world": int(last_entry["world"])
         }
     except TypeError:
         last_rating = {
-            "world": "NO_DATA",
-            "region": "NO_DATA"
+            "world": "NO_DATA"
         }
 
     position = get_world_rating(TEAM_ID)
+
+    is_changed = 1
+
     if last_rating["world"] == "NO_DATA":
         # Hvis vi mangler data
-        change = ":x:"
+        change = ":arrow_right:"
+        last_rating['world'] = position
     else:
         if position > last_rating["world"]:
             # Vi har falt
@@ -73,53 +77,42 @@ def main():
             # Vi har gått opp
             change = ":arrow_up:"
         else:
+            is_changed = 0
             # Ingen endring
             change = ":arrow_right:"
 
-    position_regional = scrape_website(TEAM_ID)
-    if last_rating["region"] == "NO_DATA":
-        change_regional = ":x:"
-    else:
-        if position_regional > last_rating["region"]:
-            change_regional = ":arrow_down:"
-        elif position_regional < last_rating["region"]:
-            change_regional = ":arrow_up:"
-        else:
-            change_regional = ":arrow_right:"
 
     time_now = pytz.timezone(
         "Europe/Oslo").localize(datetime.datetime.now().replace(microsecond=0)).isoformat()
 
     try:
         if last_entry["checked_at"] is None:
-            last_checked = "NO_DATA"
+            last_checked = time_now
         else:
             last_checked = last_entry["checked_at"]
     except TypeError:
-        last_checked = "NO_DATA"
+        last_checked = time_now
+    if is_changed == 1:
+        collection.insert_one(
+            {"checked_at": time_now, "world": position})
 
-    post_discord_message({
-        "username": "CTFTime",
-        "avatar_url": DISCORD_WEBHOOK_URL,
-        "embeds": [{
-            "title": "CTFTime ranking update",
-            "description": f"World: {change} {position}\nRegion: {change_regional} {position_regional}",
-            "timestamp": time_now,
-            "color": 11610890,
-            "fields": [{
-                "name": "Last checked",
-                "value": last_checked,
-                "inline": True
-            }, {
-                "name": "Last rating",
-                "value": f"World: {last_rating['world']}\nRegion: {last_rating['region']}",
-                "inline": True
+
+        post_discord_message({
+            "username": "CTFTime",
+            "avatar_url": PFP_URL,
+            "embeds": [{
+
+                "title": "CTFTime ranking update",
+                "description": f"World: {change} {position}\n",
+                "color": 11610890,
+                "fields": [ {
+                    "name": "Last rating",
+                    "value": f"World: {last_rating['world']}\n",
+                    "inline": True
+                }]
             }]
-        }]
-    })
+        })
 
-    collection.insert_one(
-        {"checked_at": time_now, "region": position_regional, "world": position})
 
 
 if __name__ == "__main__":
